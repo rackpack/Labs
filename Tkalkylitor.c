be4610f2da53c0c40f4b42ef058d65f6703ac7cf
rackpack <32074731+rackpack@users.noreply.github.com>
2017-12-08 10:10:42 +0300

    Create Tkalkylitor.c

---
+++ Tkalkylitor.c
@@ -0,0 +1,58 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main (int argc, char *argv[2])
+{
+	float temp;
+	float F, K, C;
+	char sys;
+	switch (argc)
+	{
+		case 2:
+			temp = atoi (argv [1]);
+			sys = 'X';
+			break;
+
+		case 3:
+			temp = atoi (argv [1]);
+			sys = *argv [2];
+			break;
+	}
+	switch (sys)
+	{
+		case 'C':
+		case 'c':
+			F = temp * 1.8 + 32.0;
+			K = temp + 273.15;
+			printf ("%.2f F\n%.2f K",F, K);
+			break;
+
+		case 'F':
+		case 'f':
+			C = (5.0/9.0) * (temp - 32.0);
+			K = (5.0/9.0) * (temp - 32.0) + 273.15;
+			printf ("%.2f C\n%.2f K",C, K);
+			break;
+
+		case 'K':
+		case 'k':
+			C = temp - 273.15;
+			F = 9.0 * (temp - 273.15)/5.0 + 32.0;
+			printf ("%.2f C\n%.2f F",C, F);
+			break;
+		default:
+			F = temp * 1.8 + 32.0;
+			K = temp + 273.15;
+			printf ("%.2f C: \n%.2f F\n%.2f K\n\n",temp, F, K);
+
+			C = (5.0/9.0) * (temp - 32.0);
+			K = (5.0/9.0) * (temp - 32.0) + 273.15;
+			printf ("%.2f F: \n%.2f C\n%.2f K\n\n",temp, C, K);
+
+			C = temp - 273.15;
+			F = 9.0 * (temp - 273.15)/5.0 + 32.0;
+			printf ("%.2f K: \n%.2f C\n%.2f F\n\n",temp, C, F);
+			break;
+	}
+	return 0;
+}
