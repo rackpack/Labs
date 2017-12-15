862f597a4a7c81e3b27988a7d4bd7190d113b537
rackpack <32074731+rackpack@users.noreply.github.com>
2017-12-15 13:02:52 +0300

    Update Конвектор.c

--- Конвектор.c
+++ Конвектор.c
@@ -22,25 +22,49 @@ int main (int argc, char *argv[2])
 	{
 		case 'C':
 		case 'c':
+			if(*argv[1]==-273.15)
+			{printf ("Абсолютный ноль");
+			 break;
+			}
+			else{
 			F = temp * 1.8 + 32.0;
 			K = temp + 273.15;
 			printf ("%.2f F\n%.2f K",F, K);
-			break;
+			break;}
 
 		case 'F':
 		case 'f':
+			if(((5.0/9.0) * (temp - 32.0))==-273.15)
+			{printf ("Абсолютный ноль");
+			 break;
+			}
+			else
+			{
 			C = (5.0/9.0) * (temp - 32.0);
 			K = (5.0/9.0) * (temp - 32.0) + 273.15;
 			printf ("%.2f C\n%.2f K",C, K);
-			break;
+			break;}
 
 		case 'K':
 		case 'k':
+			if((temp - 273.15)==-273.15)
+			{printf ("Абсолютный ноль");
+			 break;
+			}
+			else
+			{
 			C = temp - 273.15;
 			F = 9.0 * (temp - 273.15)/5.0 + 32.0;
 			printf ("%.2f C\n%.2f F",C, F);
-			break;
+			break;}
 		default:
+			if ( (*argv[1]==-273,15)&&((5.0/9.0) * ((temp - 32.0)==-273.15)&&(temp - 273.15)==(-273.15)))
+			    {
+				    printf ("Абсолютный Ноль");
+				    break;
+			    }
+			    else
+			    {
 			F = temp * 1.8 + 32.0;
 			K = temp + 273.15;
 			printf ("%.2f C: \n%.2f F\n%.2f K\n\n",temp, F, K);
@@ -52,7 +76,8 @@ int main (int argc, char *argv[2])
 			C = temp - 273.15;
 			F = 9.0 * (temp - 273.15)/5.0 + 32.0;
 			printf ("%.2f K: \n%.2f C\n%.2f F\n\n",temp, C, F);
-			break;
+			break;}
 	}
 	return 0;
 }
+
